b59c878111bdb441e8ff76c4ff1349439305eefe
AlfredHerrera <ing.alfredoherrera@hotmail.com>
2018-04-12 16:20:25 -0500

    Prueba 1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
- 'use strict';
  // Requires
  var express = require('express');
  var mysql = require('mysql');
@@ -13,29 +12,34 @@
  var con = require('./models/conexion');
 
  // Importar rutas
- var correoRutas = require('./routes/enviarCorreo');
- var ofertaRutas = require('./routes/ofertas');
- var categoriasRoutes = require('./routes/categorias');
- var productoRutas = require('./routes/producto');
- var busquedaRutas = require('./routes/busqueda');
- var appRoutes = require('./routes/app');
+ //  var correoRutas = require('./routes/enviarCorreo');
+ //  var ofertaRutas = require('./routes/ofertas');
+ //  var categoriasRoutes = require('./routes/categorias');
+ //  var productoRutas = require('./routes/producto');
+ //  var busquedaRutas = require('./routes/busqueda');
+ //  var appRoutes = require('./routes/app');
 
 
  // body parser
  // parse application/x-www-form-urlencoded
  app.use(bodyParser.urlencoded({ extended: false }));
  // si hay algo en el body que estemos enviando el body parser lo toma y lo combierte en un objeto de javascript
- app.use(bodyParser.json());
+ app.use(bodyParser.json())
 
 
  // Rutas
- app.use(require('/Correo', correoRutas));
- app.use(require('/Categorias', categoriasRoutes));
- app.use(require('/Ofertas', ofertaRutas));
- app.use(require('/Producto', productoRutas));
- app.use(require('/Busqueda', busquedaRutas));
- app.use(require('/', appRoutes));
-
+ //  app.use(require('/Correo', correoRutas));
+ //  app.use(require('/Categorias', categoriasRoutes));
+ //  app.use(require('/Ofertas', ofertaRutas));
+ //  app.use(require('/Producto', productoRutas));
+ //  app.use(require('/Busqueda', busquedaRutas));
+ //  app.use(require('/', appRoutes));
+ app.use(require('./routes/enviarCorreo'));
+ app.use(require('./routes/ofertas'));
+ app.use(require('./routes/categorias'));
+ app.use(require('./routes/producto'));
+ app.use(require('./routes/busqueda'));
+ app.use(require('./routes/app'));
 
 
 
--- models/conexion.js
+++ models/conexion.js
@@ -16,6 +16,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 module.exports = con;
