4ea8cddfd60c22a6ade4d74126d2ff545adf4e65
AlfredHerrera <ing.alfredoherrera@hotmail.com>
2018-04-12 12:20:55 -0500

    Cambio de base de datos

--- models/conexion.js
+++ models/conexion.js
@@ -1,12 +1,17 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "varfra",
-    port: '3306',
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    // host: "localhost",
+    // user: "root",
+    // password: "root",
+    // database: "varfra",
+    // port: '3306',
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    host: '173.254.61.85',
+    user: 'ventadee_equipo',
+    password: '51v.4dm1n.p455',
+    database: 'ventadee_todoenequipo',
+    port: '3306'
 });
 
 con.connect(function(err) {
