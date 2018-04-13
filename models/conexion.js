482a68a416bad84b509d8ee180e6d5174ad77c39
AlfredHerrera <ing.alfredoherrera@hotmail.com>
2018-04-13 13:21:01 -0500

    bd

--- models/conexion.js
+++ models/conexion.js
@@ -13,7 +13,7 @@ var con = mysql.createConnection({
     password: '51v.4dm1n.p455',
     database: 'ventadee_todoenequipo',
     port: '3306',
-    timeout: 60000
+    connectTimeout: 10000
 
     // connectTimeout: 20000,
     // acquireTimeout: 20000
@@ -23,6 +23,7 @@ con.connect(function(err) {
     if (err) {
         console.log(err);
     } else {
+        con.query("select e.descorta, substring(e.descrip, 1,180) descrip, e.clave, e.ruta, e.modelo, e.price, e.id from ofertas e where idStatus = 4");
         console.log('Base de datos ONLINE');
     }
 
