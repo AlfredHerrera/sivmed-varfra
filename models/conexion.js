7358049e275542d9f25ef4cc970c04170ca3c012
AlfredHerrera <ing.alfredoherrera@hotmail.com>
2018-04-13 15:43:17 -0500

    bd

--- models/conexion.js
+++ models/conexion.js
@@ -12,14 +12,20 @@ var con = mysql.createConnection({
     user: 'ventadee_equipo',
     password: '51v.4dm1n.p455',
     database: 'ventadee_todoenequipo',
-    port: '3306',
-    timeout: 20000,
-    connectTimeout: 20000,
-    acquireTimeout: 20000
+    port: '3306'
+        // timeout: 20000,
+        // connectTimeout: 20000,
+        // acquireTimeout: 20000
 });
 
-if (con.connect()) {
-    console.log('Base de datos ONLINE');
-}
+con.connect(function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        // con.query("select e.descorta, substring(e.descrip, 1,180) descrip, e.clave, e.ruta, e.modelo, e.price, e.id from ofertas e where idStatus = 4");
+        console.log('Base de datos ONLINE');
+    }
+
+});
 
 module.exports = con;
