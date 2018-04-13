02606ced926da3dc973dc1fa9cc14aca870ae21f
AlfredHerrera <ing.alfredoherrera@hotmail.com>
2018-04-13 08:53:52 -0500

    cambios

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
- // Requires
- var express = require('express');
- var mysql = require('mysql');
- var bodyParser = require('body-parser');
+// Requires
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
- // Inicializar variables
- var app = express();
+// Inicializar variables
+var app = express();
 
- const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
- // Importacion de conexion;
- var con = require('./models/conexion');
+// Importacion de conexion;
+var con = require('./models/conexion');
 
- // Importar rutas
- //  var correoRutas = require('./routes/enviarCorreo');
- //  var ofertaRutas = require('./routes/ofertas');
- //  var categoriasRoutes = require('./routes/categorias');
- //  var productoRutas = require('./routes/producto');
- //  var busquedaRutas = require('./routes/busqueda');
- //  var appRoutes = require('./routes/app');
-
-
- // body parser
- // parse application/x-www-form-urlencoded
- app.use(bodyParser.urlencoded({ extended: false }));
- // si hay algo en el body que estemos enviando el body parser lo toma y lo combierte en un objeto de javascript
- app.use(bodyParser.json())
+// Importar rutas
+var correoRutas = require('./routes/enviarCorreo');
+var ofertaRutas = require('./routes/ofertas');
+var categoriasRoutes = require('./routes/categorias');
+var productoRutas = require('./routes/producto');
+var busquedaRutas = require('./routes/busqueda');
+var appRoutes = require('./routes/app');
 
 
- // Rutas
- //  app.use(require('/Correo', correoRutas));
- //  app.use(require('/Categorias', categoriasRoutes));
- //  app.use(require('/Ofertas', ofertaRutas));
- //  app.use(require('/Producto', productoRutas));
- //  app.use(require('/Busqueda', busquedaRutas));
- //  app.use(require('/', appRoutes));
- app.use(require('./routes/enviarCorreo'));
- app.use(require('./routes/ofertas'));
- app.use(require('./routes/categorias'));
- app.use(require('./routes/producto'));
- app.use(require('./routes/busqueda'));
- app.use(require('./routes/app'));
+// body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// si hay algo en el body que estemos enviando el body parser lo toma y lo combierte en un objeto de javascript
+app.use(bodyParser.json());
+
+
+// Rutas
+app.use('/Correo', correoRutas);
+app.use('/Categorias', categoriasRoutes);
+app.use('/Ofertas', ofertaRutas);
+app.use('/Producto', productoRutas);
+app.use('/Busqueda', busquedaRutas);
+app.use('/', appRoutes);
 
 
 
- // Iniciar servidor y esperar peticiones
- app.listen(port, (err) => {
-     if (err) throw new Error(err);
-     console.log(`Express server puerto ${port}: \x1b[32m%s\x1b[0m', 'online`);
- });
+
+// Iniciar servidor y esperar peticiones
+app.listen(port, (err) => {
+    if (err) throw new Error(err);
+    console.log(`Express server puerto ${port}: \x1b[32m%s\x1b[0m', 'online`);
+});
