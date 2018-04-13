b1f8a8250456480f1625ffbe2da75698be23ab44
AlfredHerrera <ing.alfredoherrera@hotmail.com>
2018-04-13 10:39:39 -0500

    cambios

--- routes/ofertas.js
+++ routes/ofertas.js
@@ -9,19 +9,9 @@ const con = require('../models/conexion');
 // ========================
 // Obtener Equipos Nuevos Get
 // ========================
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization,   Content-Type, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    return next();
-});
 
 //  equiposNuevos
 app.get('/Ofertas', (request, response, next) => {
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization,   Content-Type, X-Requested-With");
-    response.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     con.query("select e.descorta, substring(e.descrip, 1,180) descrip, e.clave, e.ruta, e.modelo, e.price, e.id from ofertas e where idStatus = 4",
         function(err, rows) {
             response.status(200).json({ // Respuesta con codigo 200 que significa que todo esta bien 
