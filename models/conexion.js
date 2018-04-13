174d4ec8fbf3fc24b9ea875154060a6550406482
AlfredHerrera <ing.alfredoherrera@hotmail.com>
2018-04-13 12:23:20 -0500

    bd

--- models/conexion.js
+++ models/conexion.js
@@ -12,9 +12,9 @@ var con = mysql.createConnection({
     user: 'ventadee_equipo',
     password: '51v.4dm1n.p455',
     database: 'ventadee_todoenequipo',
-    port: '3306',
-    connectTimeout: 20000,
-    acquireTimeout: 20000
+    port: '3306'
+        // connectTimeout: 20000,
+        // acquireTimeout: 20000
 
 });
 
