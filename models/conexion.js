e61d4f28d7d9cfcb76d2a1b76363c159a6d35dff
Alfredo Herrera Ventura <ing.afredoherrera@gmail.com>
2018-04-30 17:53:27 -0500

    Cambios en la bd

--- models/conexion.js
+++ models/conexion.js
@@ -1,91 +1,31 @@
 /*jshint esversion: 6 */
-var mysql_npm = require('mysql');
+var mysql = require('mysql');
 
-
-
-//-
-//- Connection configuration
-//-
-var db_config = {
+var con = mysql.createConnection({
+    // host: "localhost",
+    // user: "root",
+    // password: "root",
+    // database: "varfra",
+    // port: '3306',
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
     host: '173.254.61.85',
     user: 'ventadee_equipo',
     password: '51v.4dm1n.p455',
     database: 'ventadee_todoenequipo',
     port: '3306'
-};
+        // timeout: 20000,
+        // connectTimeout: 20000,
+        // acquireTimeout: 20000
+});
 
-
-//-
-//- Create the connection variable
-//-
-var connection = mysql_npm.createPool(db_config);
-
-
-//-
-//- Establish a new connection
-//-
-connection.getConnection(function(err) {
+con.connect(function(err) {
     if (err) {
-        // mysqlErrorHandling(connection, err);
-        console.log("\n\t *** Cannot establish a connection with the database. ***");
-
-        connection = reconnect(connection);
+        console.log(err);
     } else {
-        console.log("\n\t *** New connection established with the database. ***")
-    }
-});
-
-
-//-
-//- Reconnection function
-//-
-function reconnect(connection) {
-    console.log("\n New connection tentative...");
-
-    //- Create a new one
-    connection = mysql_npm.createPool(db_config);
-
-    //- Try to reconnect
-    connection.getConnection(function(err) {
-        if (err) {
-            //- Try to connect every 2 seconds.
-            setTimeout(reconnect(connection), 2000);
-        } else {
-            console.log("\n\t *** New connection established with the database. ***")
-            return connection;
-        }
-    });
-}
-
-
-//-
-//- Error listener
-//-
-connection.on('error', function(err) {
-
-    //-
-    //- The server close the connection.
-    //-
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-        return reconnect(connection);
-    } else if (err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT") {
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-        return reconnect(connection);
-    } else if (err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR") {
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-        return reconnect(connection);
-    } else if (err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE") {
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-    } else {
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-        return reconnect(connection);
+        // con.query("select e.descorta, substring(e.descrip, 1,180) descrip, e.clave, e.ruta, e.modelo, e.price, e.id from ofertas e where idStatus = 4");
+        console.log('Base de datos ONLINE');
     }
 
 });
 
-
-//-
-//- Export
-//-
-module.exports = connection;
+module.exports = con;
